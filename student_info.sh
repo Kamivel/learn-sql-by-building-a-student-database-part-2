6f64bebdc2a678fb81c50c1da78a943c425c2593
Kamivel Kamivel <kamivel6@gmail.com>
2024-08-26 23:01:54 +0000

    Save progress: 2440.1

--- student_info.sh
+++ student_info.sh
@@ -36,3 +36,5 @@ echo "$($PSQL "SELECT major FROM majors FULL JOIN students ON majors.major_id=st
 echo -e "\nList of unique courses, in reverse alphabetical order, that no student or 'Obie Hilpert' is taking:"
 echo "$($PSQL "SELECT DISTINCT(course) FROM students RIGHT JOIN majors USING(major_id) INNER JOIN majors_courses USING(major_id) INNER JOIN courses USING(course_id) WHERE student_id
 IS NULL OR (first_name='Obie' AND last_name='Hilpert') ORDER BY course DESC")"
+
+echo -e "\nList of courses, in alphabetical order, with only one student enrolled:"
